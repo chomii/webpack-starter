7b7af3d97ec59fc3cbafc329b2f5e887871ab956
Milan Stanojevic <milanche_show@hotmail.com>
2019-09-10 14:50:59 +0200

    initial setup

---
+++ src/index.js
@@ -0,0 +1,5 @@
+
+import './index.html';
+import './index.scss';
+import './scripts/script.js';
+import 'lazysizes';
---
+++ webpack.prod.js
@@ -0,0 +1,110 @@
+const path = require('path');
+
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const TerserPlugin = require('terser-webpack-plugin');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+const ImageminWebpWebpackPlugin= require("imagemin-webp-webpack-plugin");
+const CompressionPlugin = require('compression-webpack-plugin');
+
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+var ModernizrWebpackPlugin = require('modernizr-webpack-plugin');
+
+module.exports = merge(common, {
+  mode: 'production',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              minimize: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              config: {
+                path: __dirname + '/postcss.config.js',
+                ctx: {
+                  env: 'production'
+                }
+              }
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        test: /\.js(\?.*)?$/i,
+        parallel: true,
+        sourceMap: true
+      })
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.[contentHash].css',
+      chunkFilename: '[id].css'
+    }),
+    new CompressionPlugin({
+      test: /\.(html|css|js)(\?.*)?$/i // only compressed html/css/js, skips compressing sourcemaps etc
+    }),
+    new ImageminPlugin({
+      test: /\.(jpe?g|png|gif|svg)$/i,
+      gifsicle: {
+        // lossless gif compressor
+        optimizationLevel: 9
+      },
+      pngquant: {
+        // lossy png compressor, remove for default lossless
+        quality: '75'
+      }
+    }),
+    new FaviconsWebpackPlugin({
+      logo: './src/images/logo.png',
+      // icons: {
+      //   twitter: true,
+      //   windows: true
+      // }
+    }),
+    new ModernizrWebpackPlugin({
+      'feature-detects': [
+        'img/webp'
+      ]
+    }),
+    new ImageminWebpWebpackPlugin()
+  ],
+  output: {
+    filename: '[name].[contentHash].js',
+    path: path.resolve(__dirname, 'dist')
+  }
+});
